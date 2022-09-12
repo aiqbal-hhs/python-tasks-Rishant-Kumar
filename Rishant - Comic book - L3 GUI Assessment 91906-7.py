1ecfeafd9c3d598e48cd0460f8694cfd3bd67de6
Rishant-Kumar <79290096+Rishant-Kumar@users.noreply.github.com>
2022-09-12 13:04:16 +1200

    Add files via upload

---
+++ Rishant - Comic book - L3 GUI Assessment 91906-7.py
@@ -0,0 +1,34 @@
+from cProfile import label
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+root = Tk()
+
+root.title("Comic Book Stock Manager - © Rishant")
+root.geometry("1920x1080")
+root.configure(bg="#B1DDF0")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
