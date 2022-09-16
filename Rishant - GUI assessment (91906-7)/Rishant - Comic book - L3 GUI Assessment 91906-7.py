b2b29ea5c4782d2cc89e66d50c903e724858b591
Rishant-Kumar <79290096+Rishant-Kumar@users.noreply.github.com>
2022-09-16 12:21:38 +1200

    Update Rishant - Comic book - L3 GUI Assessment 91906-7.py

--- Rishant - GUI assessment (91906-7)/Rishant - Comic book - L3 GUI Assessment 91906-7.py
+++ Rishant - GUI assessment (91906-7)/Rishant - Comic book - L3 GUI Assessment 91906-7.py
@@ -7,14 +7,6 @@ root = Tk()
 # title of window
 root.title("Comic Book Stock Manager - © Rishant")
 
-# getting devices screen information
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
-
-# setting tkinter root (main window) size
-root.geometry("%dx%d" % (width, height))
-root.configure(bg="#B1DDF0")
-
 # creating class
 class Book:
   """The Book class stores the details of each Book and has methods to Sell books, Restock books and calculates the amount of books sold and remaining stock of these books"""
@@ -54,21 +46,28 @@ water_woman = Book("Water Woman", 0, 3)
 #account_names = create_name_list()
 
 # Main GUI
-# Create the top frame
+# Create the stock level frame
 stock_level_frame = ttk.LabelFrame(root, text="Current Stock Levels")
-stock_level_frame.grid(row=10, column=115, padx=10, pady=10, sticky="NSEW")
+stock_level_frame.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
+
+# Create the stock level frame
+restock_level_frame = ttk.LabelFrame(root, text="Restock Comics")
+restock_level_frame.grid(row=10, column=115, padx=10, pady=10, sticky="NSEW")
 
 # text for each comic 
 super_dude_text = StringVar()
-super_dude_text.set("Super Dude: ")
+super_dude_text.set("Super Dude: {}"#.format()
+)
 
 lizard_man_text = StringVar()
-lizard_man_text.set("Lizard man: ")
+lizard_man_text.set("Lizard man: {}"#.format()
+)
 
 water_woman_text = StringVar()
-water_woman_text.set("Water Woman: ")
+water_woman_text.set("Water Woman: {}"#.format()
+)
 
-# Create and pack the message label
+# Create and pack the label/texts for each 
 super_dude_label = ttk.Label(stock_level_frame, textvariable=super_dude_text, wraplength=250)
 super_dude_label.grid(row=5, column=5, columnspan=2, padx=10, pady=10)
 
@@ -78,6 +77,12 @@ lizard_man_label.grid(row=10, column=5, columnspan=2, padx=10, pady=10)
 water_woman_label = ttk.Label(stock_level_frame, textvariable=water_woman_text, wraplength=250)
 water_woman_label.grid(row=15, column=5, columnspan=2, padx=10, pady=10)
 
+chosen_book_restock = StringVar()
+chosen_book_restock.set(book_list[0])
+# combobox for selecting the comic book to restock
+comic_book_restock_combobox = ttk.Combobox(restock_level_frame, textvariable=chosen_book_restock, state="readonly")
+chosen_book_restock['values'] = chosen_book_restock
+comic_book_restock_combobox.pack()
 
 
 
